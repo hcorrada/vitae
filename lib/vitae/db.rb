20866dcc99fc3792242ba940f677dc47589c7c0c
hcorrada <hcorrada@gmail.com>
2014-08-22 11:28:38 -0400

    trying mustache

--- lib/vitae/db.rb
+++ lib/vitae/db.rb
@@ -3,11 +3,28 @@ module Vitae
     attr_accessor :data
     def initialize(dbpath)
       @path = dbpath
-      @data = load_db(dbpath)
+      @data = load_db(@path)
     end
 
     def load_db(path)
-      return {name: 'Hector'}
+      read_data_to(path, @data)
     end
-  end
+
+    def read_data_to(path, data)
+      return unless File.directory?(path)
+      
+      entries = Dir.chdir(path) do
+                  Dir['*.{yaml, yml, json}'] + Dir['*'].select { |fn| File.directory?(fn) }
+                end
+
+      entries.each do |entry|
+               entry_path = File.join(path, entry)
+               key = File.basename(entry_path)
+               if File.directory?(entry_path)
+                 read_data_to(entry_path, data[key] = {})
+               else
+                 data[key] = SafeYaml.load_file(entry_path)
+               end
+             end        
+    end
 end
